f954301f852481016a5f7f2a6b9d6902fc364b9c
Gonzalo Gabriel Vega <ggvega@udc.edu.ar>
2024-08-28 12:34:53 -0300

    28-08-24 | 12:34:30

--- mensajes/views.py
+++ mensajes/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Mensaje
 
 def mensajes_recibidos(request):
-    destinatario = request.GET.get('destinatario')
+    destinatario = request.GET.get('destinatario') #obtiene el destinatario del formulario
     if destinatario:
-        mensajes = Mensaje.objects.filter(destinatario=destinatario)
+        mensajes = Mensaje.objects.filter(destinatario=destinatario).order_by('-fecha_envio') # '-fecha_envio' ordena los mensajes mas recientes
     else:
-        mensajes = Mensaje.objects.all()  # Mostrar todos los mensajes si no hay destinatario
+        mensajes = Mensaje.objects.all().order_by('-fecha_envio')  # Mostrar todos los mensajes si no hay destinatario
     return render(request, 'mensajes/recibidos.html', {'mensajes': mensajes})
